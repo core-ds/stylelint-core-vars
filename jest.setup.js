d214494db42e3804030321a3fbf238db31b568d0
Kirill Skvortsov <kiskv@outlook.com>
2024-06-07 19:37:50 +0700

    feat(*): move plugin to lib folder, tests to test folder

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,5 @@
-const getTestRule = require('jest-preset-stylelint/getTestRule');
+const { getTestRule, getTestRuleConfigs } = require('jest-preset-stylelint');
+const plugins = [require('./')];
 
-global.testRule = getTestRule({ plugins: ['./'] });
+global.testRule = getTestRule({ plugins });
+global.testRuleConfigs = getTestRuleConfigs({ plugins });
