d02a5c51acba50165669aefc1cc7b7886246d3ab
Salavat Gizatov <100ishundred@gmail.com>
2025-09-04 19:52:36 +0500

    chore(release): changeset (#6)
    * chore(release): changeset
    
    * chore(ci): step name
    
    * fix(publish): ignored files
    
    * fix(deps): range prefix
    
    * fix(deps): downgrade `@manypkg/get-packages`
    
    * fix(linter): vars check
    
    * chore(npm): ignored files
    
    * chore(lib): refactor findSubstring

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,9 @@
-const IGNORED_PACKAGES = ['@manypkg/find-root', '@manypkg/get-packages', '@manypkg/tools'];
-
 /**
  * @type {import('jest').Config}
  */
 const config = {
     preset: 'jest-preset-stylelint',
     setupFiles: ['<rootDir>/jest.setup.js'],
-    transformIgnorePatterns: [`/node_modules/(?!(${IGNORED_PACKAGES.join('|')})/)`],
 };
 
 export default config;
